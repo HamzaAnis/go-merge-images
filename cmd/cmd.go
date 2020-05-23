36c78653f2782348ded57b83898bd27c2b3c60c0
Hamza Anis <hamzaanis9514@gmail.com>
2020-05-23 10:40:48 +0500

    Proper structure

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,7 +1,30 @@
 package cmd
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/HamzaAnis/go-merge-images/utilities"
+)
 
 func Run() {
-	fmt.Println("Running")
+	paths, err := utilities.GetDirectories("/Users/macbookpro/Desktop/Programming/test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, elem := range paths {
+		println(elem)
+	}
+
+	directories, err := utilities.GetProcessedDirectories("/Users/macbookpro/Desktop/Programming/test")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, directory := range directories {
+		log.Println("For ", directory.DirectoryPath)
+		for _, file := range directory.Files {
+			log.Println(file)
+		}
+		print()
+	}
 }
--- main.go
+++ main.go
@@ -1,30 +1,8 @@
 package main
 
-import (
-	"log"
-
-	"github.com/HamzaAnis/go-merge-images/utilities"
-)
+import "github.com/HamzaAnis/go-merge-images/cmd"
 
 func main() {
-	// paths, err := utilities.GetDirectories("/Users/macbookpro/Desktop/Programming/test")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// for _, elem := range paths {
-	// 	println(elem)
-	// }
-
-	directories, err := utilities.GetProcessedDirectories("/Users/macbookpro/Desktop/Programming/test")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, directory := range directories {
-		log.Println("For ", directory.DirectoryPath)
-		for _, file := range directory.Files {
-			log.Println(file)
-		}
-		print()
-	}
+	
+	cmd.Run()
 }
